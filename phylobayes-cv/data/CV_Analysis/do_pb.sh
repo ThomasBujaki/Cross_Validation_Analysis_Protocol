5b05d6800c1b7d9794998f770dd51d83eaac62c4
ThomasBujaki <thomasbujaki@Thomass-MacBook-Air.local>
2023-01-13 15:21:13 -0500

    added hidden files to important empty folders. Changed analysis_parameters file for nucleotide level analysis. Changed do_pb.sh script to use a fixed tree file.

--- phylobayes-cv/data/CV_Analysis/do_pb.sh
+++ phylobayes-cv/data/CV_Analysis/do_pb.sh
@@ -4,6 +4,7 @@ save_freq=$(grep "save_freq" analysis_parameters | cut -f2 -d:)
 chain_length=$(grep "chain_length" analysis_parameters | cut -f2 -d:)
 nodes_used=$(grep "nodes_used" analysis_parameters | cut -f2 -d:)
 model_params=$(grep "model:" analysis_parameters | cut -f2- -d:)
+tree_file=$(grep "tree_file" analysis_parameters | awk '{print $2}')
 
 num_models=$(echo "$model_params" | wc -l)
 for i in $nodes_used ; do 
@@ -14,7 +15,7 @@ for (( i = 1; i <= $num_models ;  i++ )) {
     save_string=$(echo "$model_params" | head -n $i | tail -n1 | sed 's/^\t//g ; s/ //g ; s/^-//g ; s/-/_/g ; s/$.*\///g ; s/.txt//g' )
     model_string=$(echo "$model_params" | head -n $i | tail -n1)
     for (( rep_num = 0; rep_num < $nrep;  rep_num++ )) {
-        echo "$(pwd)/../../pb -d $(pwd)/../CV_data/${base_name}${rep_num}_learn.ali -s -x ${save_freq} ${chain_length} $(eval echo "$model_string") $(pwd)/../CV_data/${save_string}${base_name}${rep_num}_learn.ali" > submit_${save_string}_${rep_num}.sh
+        echo "$(pwd)/../../pb -d $(pwd)/../CV_data/${base_name}${rep_num}_learn.ali -T $(pwd)/../CV_data/${tree_file} -x ${save_freq} ${chain_length} $(eval echo "$model_string") $(pwd)/../CV_data/${save_string}${base_name}${rep_num}_learn.ali" > submit_${save_string}_${rep_num}.sh
         qsub -q $(echo "$queue_var" | sed 's/^,//g') -wd $(pwd)/../CV_data -S /bin/bash submit_${save_string}_${rep_num}.sh
     }
 }
